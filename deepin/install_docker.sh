9eaa29dee6e38e8c6f7de0f2598b2bcd918bb5ac
tulingfeng <xtotly@gmail.com>
2019-02-18 09:21:30 +0800

    add deepin docker install

---
+++ deepin/install_docker.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# step 1: 卸载安装过老版本
+sudo apt-get remove docker docker-engine
+# Step 2: 安装必要工具包
+sudo apt-get install apt-transport-https ca-certificates curl python-software-properties software-properties-common
+# Step 3: 更新并安装Docker-CE
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian wheezy stable"
+sudo apt-get update
+sudo apt-get install docker-ce
+# Step 4: 开启Docker服务
+sudo service docker start
+# Step 5: 配置镜像加速器
+sudo mkdir -p /etc/docker
+sudo tee /etc/docker/daemon.json <<-'EOF'
+{
+  "registry-mirrors": ["https://wr74te5j.mirror.aliyuncs.com"]
+}
+EOF
+sudo systemctl daemon-reload
+sudo systemctl restart docker
