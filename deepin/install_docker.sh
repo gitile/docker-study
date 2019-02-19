cb80742613f9ad7bbeea84d1be736cc64eed4a07
lingfeng.tu <xtotly@gmail.com>
2019-02-19 09:42:43 +0800

    Update install_docker.sh

--- deepin/install_docker.sh
+++ deepin/install_docker.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
-# step 1: Ğ¶ÔØ°²×°¹ıÀÏ°æ±¾
+# step 1: å¸è½½å®‰è£…è¿‡è€ç‰ˆæœ¬
 sudo apt-get remove docker docker-engine
-# Step 2: °²×°±ØÒª¹¤¾ß°ü
+# Step 2: å®‰è£…å¿…è¦å·¥å…·åŒ…
 sudo apt-get install apt-transport-https ca-certificates curl python-software-properties software-properties-common
-# Step 3: ¸üĞÂ²¢°²×°Docker-CE
+# Step 3: æ›´æ–°å¹¶å®‰è£…Docker-CE
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian wheezy stable"
 sudo apt-get update
 sudo apt-get install docker-ce
-# Step 4: ¿ªÆôDocker·şÎñ
+sudo gpasswd -a ${USER} docker
+# Step 4: å¼€å¯DockeræœåŠ¡
 sudo service docker start
-# Step 5: ÅäÖÃ¾µÏñ¼ÓËÙÆ÷
+# Step 5: é…ç½®é•œåƒåŠ é€Ÿå™¨
 sudo mkdir -p /etc/docker
 sudo tee /etc/docker/daemon.json <<-'EOF'
 {
-  "registry-mirrors": ["https://wr74te5j.mirror.aliyuncs.com"]
+  "registry-mirrors": ["https://registry.docker-cn.com"]
 }
 EOF
 sudo systemctl daemon-reload
